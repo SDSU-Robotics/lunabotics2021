efb2ce2572f112c91dfc3b350ee17edc468c92a0
JoelByers <joel.byers@outlook.com>
2020-09-22 10:31:55 -0500

    Added project directories, Cmake.txt and package.xml

--- README.md
+++
@@ -1,1 +0,0 @@
-# lunabotics2021
---
+++ src/hello.cpp
@@ -0,0 +1,30 @@
+#include "std_msgs/msg/string.hpp"
+#include "rclcpp/rclcpp.hpp"
+
+class Hello : public rclcpp::Node
+{
+  public:
+    Hello() : Node("pub_hello")
+    {
+      publisher = this->create_publisher<std_msgs::msg::String>("hello_topic",10);
+
+      auto message = std_msgs::msg::String();
+      message.data = "Hello There";
+
+      RCLCPP_INFO(this->get_logger(), "Publishing Hello");
+
+      publisher->publish(message);
+    }
+
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher;
+
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<Hello>());
+  rclcpp::shutdown();
+
+  return 0;
+}
