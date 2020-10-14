e4ac0c2e24a32a967cd4c5332a40d72f295b2d8b
JoelByers <joel.byers@outlook.com>
2020-10-13 19:00:42 -0500

    cleanup

--- build/lunabotics2021/ament_cmake_symlink_install_targets_0_MinSizeRel.cmake
+++
@@ -1,1 +0,0 @@
-ament_cmake_symlink_install_targets("TARGET_FILES" "F:/dev/colcon_ws/src/lunabotics2021/build/lunabotics2021/MinSizeRel/hello.exe" "TARGETS" "hello" "DESTINATION" "lib/lunabotics2021")
--- build/lunabotics2021/ament_cmake_symlink_install_targets_2_MinSizeRel.cmake
+++
@@ -1,1 +0,0 @@
-ament_cmake_symlink_install_targets("TARGET_FILES" "F:/dev/colcon_ws/src/lunabotics2021/build/lunabotics2021/MinSizeRel/joystick_ros2.exe" "TARGETS" "joystick_ros2" "DESTINATION" "lib/lunabotics2021")
