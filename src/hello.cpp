3d72e0c4970cd26e29adeda0e879ecd70fa96a6a
JoelByers <joel.byers@outlook.com>
2020-10-16 18:31:32 -0500

    Added dlls

--- src/hello.cpp
+++ src/hello.cpp
@@ -1,4 +1,4 @@
-//#include "std_msgs/msg/string.hpp"
+#include "geometry_msgs/msg/twist.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "ctre/Phoenix.h"
 #include "ctre/phoenix/platform/Platform.h"
@@ -11,32 +11,45 @@ using namespace ctre::phoenix::platform;
 using namespace ctre::phoenix::motorcontrol;
 using namespace ctre::phoenix::motorcontrol::can;
 
+using std::placeholders::_1;
+
 class Hello : public rclcpp::Node
 {
   public:
     Hello() : Node("pub_hello")
     {
-
-      //RCLCPP_INFO(this->get_logger(), "Publishing Hello");
+      subscriber = this->create_subscription<geometry_msgs::msg::Twist>(
+        "hello_topic", 10, std::bind(&Hello::topic_callback, this, _1));
+      
+      RCLCPP_INFO(this->get_logger(), "Publishing Hello");
 
     }
 
-    TalonSRX talon = 1;
+    private:
 
+    void topic_callback(const geometry_msgs::msg::Twist msg) const
+    {
+      RCLCPP_INFO(this->get_logger(), "I heard");
+    }
+
+
+    rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr subscriber;
 };
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::Rate loopRate(10);
+  //rclcpp::Rate loopRate(10);
+
+  TalonSRX talon = 1;
 
 	ctre::phoenix::platform::can::SetCANInterface("can0");
-  //ctre::phoenix::platform::can::StartAll();
 
   while(rclcpp::ok())
   {
-    rclcpp::spin_some(std::make_shared<Hello>());
-    loopRate.sleep();
+    rclcpp::spin(std::make_shared<Hello>());
+
+    //loopRate.sleep();
   }
   
   rclcpp::shutdown();
