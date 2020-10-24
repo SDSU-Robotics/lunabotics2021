3e368802cf148349f5fc3e4a51e61e668bb67f25
JoelByers <joel.byers@outlook.com>
2020-10-23 20:35:29 -0500

    dep

--- src/hello.cpp
+++ src/hello.cpp
@@ -45,7 +45,7 @@ int main(int argc, char * argv[])
   rclcpp::init(argc, argv);
   auto node = std::make_shared<Hello>();
 
-  TalonSRX talon = 2;
+  TalonSRX talon(2);
 
 	ctre::phoenix::platform::can::SetCANInterface("can0");
 
